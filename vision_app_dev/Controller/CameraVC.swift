8bcd156722c4dd5185235d439f0dab01b44eec52
Krystyna Kruchkovska <kruchok883@gmail.com>
2018-11-12 17:04:07 +0200

    IntoML

---
+++ vision_app_dev/Controller/CameraVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vision_app_dev
+//
+//  Created by Mac on 11/12/18.
+//  Copyright © 2018 CO.KrystynaKruchcovska. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
